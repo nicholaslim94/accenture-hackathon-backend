e51ff71dd1b6e816b5d49550dd0ce684e0a9cffc
nicholaslim94 <nicholaslim.pc94@gmail.com>
2021-07-27 12:55:17 +0800

    first commit

---
+++ src/main/java/com/example/vaccinatetogether/VaccinateTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.example.vaccinatetogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinateTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinateTogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vaccinatetogether/VaccinateTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaccinatetogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinateTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
